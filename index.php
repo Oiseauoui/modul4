a406b44240706d1aa16798dd48ee8f0e2a2cd7b7
Shishik Julia <oiseua@gmail.com>
2017-06-02 17:32:04 +0300

    test

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+define('ROOT',dirname(__FILE__));
+define('DS',DIRECTORY_SEPARATOR);
+define('VIEW_PATH',ROOT.DS.'views');
+define('ALLOW_TYPES',[
+    'image/jpeg'=>'jpg',
+    'image/gif'=>'gif',
+    'image/png'=>'png'
+]);
+include ROOT.DS.'core'.DS.'load.class.php';
+
+session_start();
+App::run($_SERVER['REQUEST_URI']);
